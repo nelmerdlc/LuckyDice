01c1ae1b014c31fbc247ebcad831e8bd6dd4e6e0
Nelmer De La Cruz <nelmerux@gmail.com>
2019-06-04 14:58:54 -0400

    views ready to link

--- app/src/main/java/design/nelmer/luckydice/MainActivity.kt
+++ app/src/main/java/design/nelmer/luckydice/MainActivity.kt
@@ -2,11 +2,14 @@ package design.nelmer.luckydice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.btn_roll)
+//        rollButton.text = "Let's Roll!"
     }
 }
