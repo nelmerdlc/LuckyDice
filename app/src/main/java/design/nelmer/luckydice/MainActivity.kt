7a60a0c160ad7e47eacb546dd4179d2750801aea
Nelmer De La Cruz <nelmerux@gmail.com>
2019-07-05 17:36:21 -0400

    Random function working properly

--- app/src/main/java/design/nelmer/luckydice/MainActivity.kt
+++ app/src/main/java/design/nelmer/luckydice/MainActivity.kt
@@ -3,6 +3,8 @@ package design.nelmer.luckydice
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.btn_roll)
-//        rollButton.text = "Let's Roll!"
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val diceResult: TextView = findViewById(R.id.tv_diceResult)
+        val randomInt = Random().nextInt(6) + 1
+
+        diceResult.text = randomInt.toString()
     }
 }
